1e489c9fbbdf7bf12643283cb5e8ffc226b6480c
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-07-12 03:03:04 +0500

    solve routing issues

--- login/login.js
+++ login/login.js
@@ -7,9 +7,9 @@ isLoggedIn = isLoggedIn ? JSON.parse(isLoggedIn) : false;
 window.onload = function () {
   console.log("Page loaded.");
   console.log("Login status:", isLoggedIn);
-  if (isLoggedIn && !window.location.pathname.includes("/index.html")) {
+  if (isLoggedIn && !window.location.pathname.includes("/")) {
     console.log("Redirecting to index.html");
-    window.location.href = "/index.html";
+    window.location.href = "/";
   } else if (
     !isLoggedIn &&
     !window.location.pathname.includes("/login/login.html")

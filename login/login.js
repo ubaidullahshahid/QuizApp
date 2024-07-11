c2fa01cec59e822092b69eb8b6c3ec80a165f437
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-07-12 03:25:36 +0500

    solve issues

--- login/login.js
+++ login/login.js
@@ -7,9 +7,10 @@ isLoggedIn = isLoggedIn ? JSON.parse(isLoggedIn) : false;
 window.onload = function () {
   console.log("Page loaded.");
   console.log("Login status:", isLoggedIn);
-  if (isLoggedIn && !window.location.pathname.includes("/")) {
+  console.log(window.location.pathname.includes("/login/login.html"))
+  if (isLoggedIn && !window.location.pathname.includes("/index.html")) {
     console.log("Redirecting to index.html");
-    window.location.href = "/";
+    window.location.href = "/index.html";
   } else if (
     !isLoggedIn &&
     !window.location.pathname.includes("/login/login.html")
